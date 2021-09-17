785e4ff24aa99a6e8320296bd15a99711a17acc6
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2021-09-17 16:51:48 +0530

    Create EQUALSIGNORECASE.java

---
+++ EQUALSIGNORECASE.java
@@ -0,0 +1,7 @@
+public class EQUALSIGNORECASE{
+    public static void main(String args[]){
+        String s1 = "vinsmoke";
+        String s2 = "sanji";
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+}
