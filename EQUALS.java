129fb0c1ed4f374fc23953d32e7fbc49ec7cbd3c
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2021-09-17 16:50:34 +0530

    Create EQUALS.java

---
+++ EQUALS.java
@@ -0,0 +1,7 @@
+public class EQUALS{
+    public static void main(String args[]){
+        String s1 = "vinsmoke";
+        String s2 = "sanji";
+        System.out.println(s1.equals(s2));
+    }
+}
