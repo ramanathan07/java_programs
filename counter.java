f9a780b20f2eba137e7afb23b25743d6eca2bd53
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2021-09-19 08:19:04 +0530

    Create counter.java

---
+++ counter.java
@@ -0,0 +1,13 @@
+//Program of counter without static variable
+class Counter{ 
+int count=0;//will get memory when instance is created 
+ Counter(){ 
+count++; 
+System.out.println(count); 
+}  
+public static void main(String args[]){ 
+ Counter c1=new Counter(); 
+Counter c2=new Counter(); 
+Counter c3=new Counter(); 
+ } 
+}
