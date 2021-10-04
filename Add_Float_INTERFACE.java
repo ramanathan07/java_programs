0799b4be5e6b7d84d259c4c6c719e93664e1d741
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2021-10-04 10:42:54 +0530

    Update and rename Add_Float.java to Add_Float_INTERFACE.java

--- Add_Float_INTERFACE.java
+++ Add_Float_INTERFACE.java
@@ -3,7 +3,7 @@ class add
   float x=666.f;
   float y=0235.f;
 }
-public class Add_Float extends add
+public class Add_Float_INTERFACE extends add
 {
     float z=x+y;
     public static void main(String args[])
