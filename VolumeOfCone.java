3a8df41336c65c627c4928271c68a54bc64d1bed
ramanathan07 <89630494+ramanathan07@users.noreply.github.com>
2021-10-09 21:15:25 +0530

    Create VolumeOfCone.java

---
+++ VolumeOfCone.java
@@ -0,0 +1,10 @@
+public class VolumeOfCone{  
+    public static void main(String args[])  
+    {  
+    int H=4;  
+    int r=2;  
+    double Pie=3.14285714286;  
+    double Volume=Pie*(r*r)*H/3;  
+        System.out.println("The volume of cone is"+Volume);  
+     }  
+}  
